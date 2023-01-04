879a675f62d444a02f92c1599c00786ec0abeb05
Lakna Attigala <Lakna0804@gmail.com>
2023-01-04 15:25:32 +0530

    -> second commit

---
+++ app/src/main/java/lk/viva/publiclibraries/BookListActivity.java
@@ -0,0 +1,75 @@
+package lk.viva.publiclibraries;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.ClipData;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+
+import java.util.ArrayList;
+
+public class BookListActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    DatabaseReference database;
+    BooksAdapter booksAdapter;
+    ArrayList<Books>list;
+    StorageReference storageReference;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_list);
+        // Assign FirebaseStorage instance to storageReference.
+        storageReference = FirebaseStorage.getInstance().getReference();
+        recyclerView = findViewById(R.id.booklist);
+        database = FirebaseDatabase.getInstance().getReference("books");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        list = new ArrayList<>();
+        booksAdapter = new BooksAdapter(this,list);
+        recyclerView.setAdapter(booksAdapter);
+
+
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for ( DataSnapshot  dataSnapshot: snapshot.getChildren())
+                {
+                    Books books = dataSnapshot.getValue(Books.class);
+                    list.add(books);
+                }
+                booksAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/lk/viva/publiclibraries/Books.java
@@ -0,0 +1,17 @@
+package lk.viva.publiclibraries;
+
+public class Books {
+    String bookimage,title,publisher;
+
+    public String getBookimage() {
+        return bookimage;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+}
